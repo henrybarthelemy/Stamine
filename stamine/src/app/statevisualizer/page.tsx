3f2063bfbd955d7d9b5bfe837e0f6514c14c3588
henrybarthelemy <henry.v.barthelemy@gmail.com>
2023-11-25 20:19:25 -0500

    Added stamine page and form

---
+++ stamine/src/app/statevisualizer/page.tsx
@@ -0,0 +1,173 @@
+
+'use client';
+import React, { useEffect, useState } from 'react';
+import cytoscape from 'cytoscape';
+import "./style.css";
+
+const StateVisualizer = () => {
+    const [cy, setCy] = useState(null);
+    const [nodeValue, setNodeValue] = useState<string>('');
+    useEffect(() => {
+        // Initialize Cytoscape
+        const newCy = cytoscape({
+            container: document.getElementById('cy'),
+            elements: [
+                // nodes
+                { data: { id: 'head-1', label: '1' } },
+                { data: { id: 'item-2', label: '2' } },
+                { data: { id: 'item-3', label: '3' } },
+
+                // edges
+                { data: { id: 'edge-1', source: 'head-1', target: 'item-2' } },
+                { data: { id: 'edge-2', source: 'head-1', target: 'item-3' } },
+                // edges can be removed if not needed
+            ],
+            style: [
+                {
+                    selector: 'node:active',
+                    style: {
+                        'overlay-padding': 0,
+                        'overlay-color': 'transparent',
+                        'overlay-opacity': 0,
+                    }
+                },
+                {
+                    selector: 'node:selected',
+                    style: {
+                        'border-width': 0,
+                        'overlay-padding': 0,
+                        'overlay-color': 'transparent',
+                        'overlay-opacity': 0,
+                    }
+                },
+                {
+                    selector: 'node',
+                    style: {
+                        'shape': 'round-rectangle',
+                        'background-color': '#666',
+                        'label': 'data(label)',
+                        'text-valign': 'center',
+                        'text-halign': 'center',
+                        'overlay-opacity': 0,
+                        'border-width': 0,
+                        'width': 50,
+                        'height': 50,
+                        'cursor': 'default'
+                    }
+                },
+                {
+                    selector: 'edge',
+                    style: {
+                        'width': 3,
+                        'line-color': '#ccc'
+                    }
+                },
+                {
+                    selector: 'core',
+                    style: {
+                        'active-bg-opacity': 0,
+                        'active-bg-color': 'transparent',
+                        'active-bg-size': 0,
+
+
+
+                    }
+                },
+                {
+                    selector: 'edge',
+                    style: {
+                        'overlay-padding': 0,
+                        'overlay-color': 'transparent',
+                        'overlay-opacity': 0,
+                    }
+                }
+            ],
+            layout: {
+                name: 'grid',
+                fit: true
+            },
+            // Interaction options
+            userZoomingEnabled: false,
+            userPanningEnabled: false,
+            boxSelectionEnabled: false,
+        });
+
+        newCy.nodes().ungrabify();
+        setCy(newCy);
+        // Function to handle resize
+        const handleResize = () => {
+            newCy.resize();
+            applyTreeLayout(newCy);
+        };
+
+        // Add event listener for resize
+        window.addEventListener('resize', handleResize);
+
+        // Apply initial layout
+        applyTreeLayout(newCy);
+
+        // Remove event listener on cleanup
+        return () => {
+            window.removeEventListener('resize', handleResize);
+            newCy.destroy();
+        };
+    }, []);
+
+    // Function to layout nodes in a horizontal line
+    const applyTreeLayout = (cyInstance) => {
+        const nodes = cyInstance.nodes();
+        const containerWidth = cyInstance.container().offsetWidth;
+        const containerHeight = cyInstance.container().offsetHeight;
+        let depth = 1;
+
+        nodes.forEach((cur) => {
+
+        });
+
+        // Instead of fit, manually set zoom and pan
+        if (nodes.length === 1) {
+            // If only one node, center it without zooming in
+            cyInstance.zoom({
+                level: 1, // Set the zoom level to 1 (or any appropriate level)
+                renderedPosition: { x: containerWidth / 2, y: containerHeight / 2 }
+            });
+        } else {
+            // If more than one node, fit them to the screen with padding
+            cyInstance.fit(nodes, 50); // Include 50 pixels of padding
+        }
+
+        cyInstance.center(nodes); // Center the nodes in the viewport
+    };
+
+
+    return (
+        <div>
+            <div className="title">
+                <p>Stamine</p>
+            </div>
+
+            <div className="cyContainer">
+                <div id='cy' className="bg-blue-500" />
+            </div>
+
+            <div className="fullParent">
+                <div className="halfChild">
+                    <div className="description">
+                        <p>
+                            Here I can describe what the user must do
+                        </p>
+                    </div>
+                </div>
+                <div className="halfChild">
+                    <p> Here I can have some input forms</p>
+                    <form id="yourFormId">
+                        <label htmlFor="inputField">Input Field:</label>
+                        <input type="text" id="inputField" name="inputField" required/>
+                        <button type={"submit"}>Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+};
+export default StateVisualizer;
